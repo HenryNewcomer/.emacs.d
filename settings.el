5217683c7c790a86b69a7bde97160c62368684e9
HenryNewcomer <a.cliche.email@gmail.com>
2018-10-30 20:59:27 -0500

    Switched to emacs's internal relative line number method

--- settings.el
+++ settings.el
@@ -52,12 +52,21 @@
 ;;(add-to-list 'load-path "~/.emacs.d/from_backup/undo-tree")
 (global-undo-tree-mode)
 
-(require 'nlinum-relative)
-(nlinum-relative-setup-evil)                    ;; setup for evil
-(add-hook 'prog-mode-hook 'nlinum-relative-mode)
-(setq nlinum-relative-redisplay-delay 0)        ;; delay
-(setq nlinum-relative-current-symbol "")        ;; "" to display current line number (was "->")
-(setq nlinum-relative-offset 0)                 ;; 1 if you want 0, 2, 3...
+;;(setq display-line-numbers 'relative) 
+(setq-default display-line-numbers 'relative
+	      display-line-numbers-type 'visual
+              display-line-numbers-current-absolute t
+              display-line-numbers-width 4
+              display-line-numbers-widen t)
+(add-hook 'text-mode-hook #'display-line-numbers-mode)
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+  ;; (require 'nlinum-relative)
+  ;; (nlinum-relative-setup-evil)                    ;; setup for evil
+  ;; (add-hook 'prog-mode-hook 'nlinum-relative-mode)
+  ;; (setq nlinum-relative-redisplay-delay 0)        ;; delay
+  ;; (setq nlinum-relative-current-symbol "")        ;; "" to display current line number (was "->")
+  ;; (setq nlinum-relative-offset 0)                 ;; 1 if you want 0, 2, 3...
 
 ;; Check OS
 (cond
@@ -90,13 +99,32 @@
 (set-buffer-file-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
-(set-clipboard-coding-system 'utf-16le)
 (set-file-name-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;;; Source; https://www.emacswiki.org/emacs/ForceBackups
+;; Default and per-save backups go here:
+(setq backup-directory-alist '(("" . "~/.emacs.d/backup/per-save")))
+
+(defun force-backup-of-buffer ()
+;; Make a special "per session" backup at the first save of each
+;; emacs session.
+(when (not buffer-backed-up)
+    ;; Override the default parameters for per-session backups.
+    (let ((backup-directory-alist '(("" . "~/.emacs.d/backup/per-session")))
+	(kept-new-versions 3))
+    (backup-buffer)))
+;; Make a "per save" backup on each save.  The first save results in
+;; both a per-session and a per-save backup, to keep the numbering
+;; of per-save backups consistent.
+(let ((buffer-backed-up nil))
+    (backup-buffer)))
+
+(add-hook 'before-save-hook  'force-backup-of-buffer)
 
 (define-key evil-normal-state-map (kbd "SPC") nil)
 ;;(define-key evil-insert-state-map (kbd "SPC") (kbd "SPC"))
@@ -113,11 +141,13 @@
 (define-key evil-normal-state-map (kbd "SPC h") 'split-window-below)
 (define-key evil-normal-state-map (kbd "SPC v") 'split-window-right)
 
-;;(windmove-default-keybindings 'meta)
-;;(global-set-key (kbd "SPC j") #'other-window)
+(define-key evil-normal-state-map (kbd "C-M-h") 'windmove-left)
+(define-key evil-normal-state-map (kbd "C-M-l") 'windmove-right)
+(define-key evil-normal-state-map (kbd "C-M-k") 'windmove-up)
+(define-key evil-normal-state-map (kbd "C-M-j") 'windmove-down)
+
 (define-key evil-normal-state-map (kbd "SPC j") #'other-window)
 (define-key evil-normal-state-map (kbd "SPC k") #'prev-window)
-
 (defun prev-window ()
   (interactive)
   (other-window -1))
@@ -127,6 +157,8 @@
 (define-key evil-normal-state-map (kbd "SPC t") 'term)
 
 (define-key evil-normal-state-map (kbd "SPC w") 'save-buffer)
+
+(define-key evil-normal-state-map (kbd "SPC q") 'save-buffers-kill-emacs)
 
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C-_") 'text-scale-decrease)
